8eae1ea75a59e1cd4c7f271ae4e667f949155039
Carloto <viniciuscarloto@gmail.com>
2022-03-28 18:40:49 -0300

    add data fetch and display test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,41 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from '@testing-library/react';
+import Home from '@/pages/index';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const server = setupServer(
+  rest.get('https://api.coingecko.com/api/v3/exchanges', (req, res, ctx) => {
+    const page = req.url.searchParams.get('page') as '1' | '2';
+
+    const exchanges = { '1': [{ id: 'binance' }, { id: 'kucoin' }], '2': {} };
+
+    return res(ctx.json(exchanges[page]));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+  it('renders the page', () => {
+    render(<Home />);
+  });
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+  it('fetches and displays exchanges list', async () => {
+    render(<Home />);
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    await screen.findByText('binance');
+    await screen.findByText('kucoin');
+  });
+
+  // it('renders a heading', () => {
+  //   render(<Home />);
+
+  //   const heading = screen.getByRole('heading', {
+  //     name: /welcome to next\.js!/i,
+  //   });
+
+  //   expect(heading).toBeInTheDocument();
+  // });
+});
